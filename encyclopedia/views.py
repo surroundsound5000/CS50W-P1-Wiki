b934aabc19d67169d2e931a240fce5a185144b00
surroundsound5000 <44852447+surroundsound5000@users.noreply.github.com>
2023-12-16 19:13:40 +1100

    Entry Page: Visiting /wiki/TITLE, where TITLE is the title of an encyclopedia entry, should render a page that displays the contents of that encyclopedia entry.
        The view should get the content of the encyclopedia entry by calling the appropriate util function.
        If an entry is requested that does not exist, the user should be presented with an error page indicating that their requested page was not found.
        If the entry does exist, the user should be presented with a page that displays the content of the entry. The title of the page should include the name of the entry.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 from . import util
-
+from . import markdown2
 
 def index(request):
+    print(request)
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    try: return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "body": markdown2.markdown(entry)
+        })
+    except: return render(request, "encyclopedia/notfound.html", {
+        "title" : title
+    })
