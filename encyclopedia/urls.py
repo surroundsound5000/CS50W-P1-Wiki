838e3d7491329bca6acd38e9ba511fd5068c0edc
surroundsound5000 <44852447+surroundsound5000@users.noreply.github.com>
2023-12-16 20:43:54 +1100

    Implemented search
    Search: Allow the user to type a query into the search box in the sidebar to search for an encyclopedia entry.
    
        If the query matches the name of an encyclopedia entry, the user should be redirected to that entry’s page.
        If the query does not match the name of an encyclopedia entry, the user should instead be taken to a search results page that displays a list of all encyclopedia entries that have the query as a substring. For example, if the search query were ytho, then Python should appear in the search results.
        Clicking on any of the entry names on the search results page should take the user to that entry’s page.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>/", views.entry)
+    path("wiki/<str:title>/", views.entry),
+    path("search", views.search, name = "search")
 ]
