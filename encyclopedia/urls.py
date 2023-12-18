83fa74efefb76d25198290e8ab8c5f1bff186bce
surroundsound5000 <44852447+surroundsound5000@users.noreply.github.com>
2023-12-18 13:26:56 +1100

    Implemented Edit Page
    Edit Page: On each entry page, the user should be able to click a link to be taken to a page where the user can edit that entry’s Markdown content in a textarea.
    
        The textarea should be pre-populated with the existing Markdown content of the page. (i.e., the existing content should be the initial value of the textarea).
        The user should be able to click a button to save the changes made to the entry.
        Once the entry is saved, the user should be redirected back to that entry’s page.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("wiki/<str:title>/", views.entry),
     path("search", views.search, name = "search"),
     path("new", views.new, name = "new"),
-    path("save", views.save, name="save")
+    path("save", views.save_new, name="save"),
+    path("edit", views.edit, name="edit"),
+    path("save_edit", views.save_edit, name="save_edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -37,7 +37,7 @@ def search(request):
 def new(request):
     return render(request, "encyclopedia/new.html")
 
-def save(request):
+def save_new(request):
     title = request.GET.get("title")
     entries = util.list_entries()
     if title in entries:
@@ -45,5 +45,21 @@ def save(request):
             "error" : ["A page with this title already exists."],
         })
     content = "#" + title + "\n\n" + request.GET.get("content")
+    util.save_entry(title, content)
+    return entry(request, title)
+
+def edit(request):
+    title = request.GET.get("title")
+    this_entry = util.get_entry(title)
+    return render(request, "encyclopedia/edit.html", {
+            "title": title,
+            "body": this_entry
+        })
+
+
+def save_edit(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+    print("Title=", title, "\nContent=", content)
     util.save_entry(title, content)
     return entry(request, title)
